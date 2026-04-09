6e32f5a2cc163c0434815fb5f03c618bf7d1b575
Vlad <makarvlad2014@yandex.ru>
2026-04-09 08:38:12 +0300

    firts commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Translate(input string) (string, error) {
+	trimmed := strings.TrimSpace(input)
+	if trimmed == "" {
+		return "", fmt.Errorf("input err")
+	}
+
+	if isMorse(trimmed) {
+		result := morse.ToText(trimmed)
+		if result == "" && trimmed != "" {
+			return "", fmt.Errorf("conv to text err")
+		}
+		return result, nil
+	}
+	result := morse.ToMorse(trimmed)
+	if strings.TrimSpace(result) == "" && strings.TrimSpace(trimmed) != "" {
+		return "", fmt.Errorf("conv to morse err")
+	}
+	return result, nil
+}
+func isMorse(s string) bool {
+	for _, ch := range s {
+		if ch != ' ' && ch != '.' && ch != '-' {
+			return false
+		}
+	}
+	return true
+}
